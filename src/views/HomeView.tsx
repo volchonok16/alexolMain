0e266c77695e8bbbc43860ffcf7df90eeb440078
ximera405 <kapustka633@gmail.com>
2024-09-24 20:30:47 +0300

    connect tailwind

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -2,7 +2,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const HomeView = () => {
 
-    return <div className=""></div>;
+    return <div className="w-100 h-100 bg-white absolute"></div>;
 };
 
 export default HomeView;
