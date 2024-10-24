50862c750cdf3cd2f049a66177ff0719df1a42b4
Anna Shatilova <anxet@mail.ru>
2024-10-24 14:27:48 +0300

    Корректировка слайдов в блоке Парнеры

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -12,19 +12,14 @@ import { DividerSolid } from "@/widgets/DividerSolid";
 
 import VkLogo from "../assets/vk.svg";
 import WhatsAppLogo from "../assets/whatsapp.svg";
+import ViberLogo from "../assets/viber.svg";
 import InstLogo from "../assets/inst.svg";
 import TgLogo from "../assets/tg.svg";
 import { Link } from "react-router-dom";
-import { DividerDots } from "@/widgets/DividerDots";
 
 export const Partners = () => {
   return (
     <section className="mt-10">
-      <div className="flex flex-col items-center mt-12 mb-10">
-        <DividerDots className="mb-10" />
-        <p className="text-[48px] leading-normal">Наши партнеры</p>
-        <DividerSolid className="w-[130px]" />
-      </div>
       <Carousel
         responsive={{
           desktop: {
@@ -68,29 +63,50 @@ export const Partners = () => {
         {partnersInfo.map((el) => {
           return (
             <div
-              className="flex justify-between text-right px-20 py-5 rounded-2xl"
+              className="flex justify-between text-right px-10 py-5 rounded-2xl"
               style={{
                 background: `url(${bg})`,
                 backgroundSize: "100% 100%",
-                height: "530px",
+                height: "560px",
               }}
             >
               <div>
                 <img className="w-[600px]" src={el.img} />
               </div>
-              <div className="flex flex-col items-end w-[450px]">
+              <div className="flex flex-col items-end w-[500px]">
+                <div className="flex flex-col items-end">
+                  <p className="text-[48px] leading-normal">Наши партнеры</p>
+                  <DividerSolid className="w-[130px]" />
+                </div>
                 <img src={el.logo} />
-                <DividerSolid className="my-10" />
-                <p className="text-[24px]">{el.title}</p>
-                <p className="mt-5">{el.desc}</p>
+                <p className="text-[26px] font-medium">{el.title}</p>
+                <p className="text-[24px] font-normal mt-5">{el.desc}</p>
                 <p className="text-[24px] cursor-pointer mt-5">{el.mail}</p>
-                <div className="flex justify-end gap-4 mt-5">
+                <div className="flex justify-end items-center gap-4 mt-5">
+                  <Link to={el.site} target="_blank">
+                    <button
+                      className="py-2 px-4 bg-gradient-to-r rounded-lg text-white"
+                      style={{
+                        background:
+                          "linear-gradient(0.25turn, #97794D, #E3CB8F)",
+                      }}
+                    >
+                      Узнать подробнее
+                    </button>
+                  </Link>
                   <Link to={el.socials.vk} target="_blank">
                     <VkLogo className="cursor-pointer" />
                   </Link>
-                  <Link to={el.socials.wa} target="_blank">
-                    <WhatsAppLogo className="cursor-pointer" />
-                  </Link>
+                  {el.socials.wa && (
+                    <Link to={el.socials.wa} target="_blank">
+                      <WhatsAppLogo className="cursor-pointer" />
+                    </Link>
+                  )}
+                  {el.socials.viber && (
+                    <Link to={el.socials.viber} target="_blank">
+                      <ViberLogo className="cursor-pointer" />
+                    </Link>
+                  )}
                   <Link to={el.socials.inst} target="_blank">
                     <InstLogo className="cursor-pointer" />
                   </Link>
@@ -98,16 +114,6 @@ export const Partners = () => {
                     <TgLogo className="cursor-pointer" />
                   </Link>
                 </div>
-                <Link to={el.site} target="_blank">
-                  <button
-                    className="py-2 px-4 bg-gradient-to-r rounded-lg text-white mt-4"
-                    style={{
-                      background: "linear-gradient(0.25turn, #97794D, #E3CB8F)",
-                    }}
-                  >
-                    Узнать подробнее
-                  </button>
-                </Link>
               </div>
             </div>
           );
@@ -129,9 +135,9 @@ const partnersInfo = [
     socials: {
       vk: "https://vk.com/it.incubator?roistat_visit=244395",
       wa: "https://api.whatsapp.com/send/?phone=375445657493&text&type=phone_number&app_absent=0&roistat_visit=245016",
+      viber: "viber://chat?number=%2B375291341548",
       inst: "https://www.instagram.com/it.incubator?roistat_visit=245016",
       tg: "https://t.me/ITIncubatorSandbox?roistat_visit=245016",
-      viber: "viber://chat?number=%2B375291341548",
     },
   },
   {
@@ -154,12 +160,11 @@ const partnersInfo = [
     img: pranaImg,
     logo: pranaLogo,
     title: "Центр психологии PRANA",
-    desc: "Семейная, кризисная психология, психосоматика, псхоанализ, коучинг",
+    desc: "Семейная, кризисная психология, психосоматика, психоанализ, коучинг. Быстрая помощь в решении проблемных ситуаций.Различные виды консультаций.",
     mail: "prana.vedic.center@gmail.com",
     site: "https://prana-psychology.ru/",
     socials: {
       vk: "https://vk.com/prana_vedic_center",
-      wa: "https://wa.me/79082227821",
       inst: "https://instagram.com/prana_vedic_centr?igshid=MzRlODBiNWFlZA==",
       tg: "https://tg.me",
     },
