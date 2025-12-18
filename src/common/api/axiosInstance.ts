d0199ee94ab0963fd504b23db120f0e337d8bcf8
ximera405 <kapustka633@gmail.com>
2025-12-18 22:18:34 +0300

    new design

--- src/common/api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-// Create an Axios instance with default options
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/",
-    timeout: 10000,
-});
-
-export default axiosInstance;
