89182520b316273f8792ff02eb74e6b631856a1c
avolc <taraskin1610@gmail.com>
2026-03-09 23:35:34 +0300

    change_news

--- backend/bot/config.py
+++ backend/bot/config.py
@@ -160,6 +160,6 @@ MAX_POST_LENGTH = 3000
 # Alexol | Подписаться (https://t.me/+QofDQ2Ctq8o1Y2Ey)
 SUBSCRIBE_FOOTER = os.getenv(
     "SUBSCRIBE_FOOTER",
-    "Alexol(https://t.me/+QofDQ2Ctq8o1Y2Ey) |(https://t.me/+QofDQ2Ctq8o1Y2Ey) Подписаться:(https://t.me/+QofDQ2Ctq8o1Y2Ey)", parse_mode=ParseMode.MARKDOWN
+    "Alexol | Подписаться (https://t.me/+QofDQ2Ctq8o1Y2Ey)",
 )
 
